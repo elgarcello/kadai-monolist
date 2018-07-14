b4abbc59a80e81382a2d52e37de82d072b9cc5a1
elgar_cello_du_pre <du_pre_elgar@asagi.waseda.jp>
2018-07-14 10:22:30 +0000

    for heroku

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,13 @@
 class Item < ApplicationRecord
-  validates :code, presence: true, length: { maximum: 255 }
+  validates :code, presence: true, uniqueness: true, length: { maximum: 255 }
   validates :name, presence: true, length: { maximum: 255 }
   validates :url, presence: true, length: { maximum: 255 }
   validates :image_url, presence: true, length: { maximum:255 }
   
-  has_many :ownerships
-  has_many :users, through: :ownerships
-  has_many :wants
-  has_many :wants_users, through: :wants, class_name: 'User', source: :user
+  has_many :ownerships, dependent: :destroy
+  has_many :users, through: :ownerships, dependent: :destroy
+  has_many :wants, dependent: :destroy
+  has_many :wants_users, through: :wants, class_name: 'User', source: :user, dependent: :destroy
+  has_many :haves, class_name: 'Have', dependent: :destroy
+  has_many :have_users, through: :haves, class_name: 'User', source: :user, dependent: :destroy
 end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,12 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   
-  has_many :ownerships
-  has_many :items, through: :ownerships
-  has_many :wants
-  has_many :want_items, through: :wants, class_name: 'Item', source: :item
+  has_many :ownerships, dependent: :destroy
+  has_many :items, through: :ownerships, dependent: :destroy
+  has_many :wants, dependent: :destroy
+  has_many :want_items, through: :wants, class_name: 'Item', source: :item, dependent: :destroy
+  has_many :haves, class_name: 'Have', dependent: :destroy
+  has_many :have_items, through: :haves, class_name: 'Item', source: :item, dependent: :destroy
   
   def want(item)
     self.wants.find_or_create_by(item_id: item.id)
@@ -22,6 +24,19 @@ class User < ApplicationRecord
   
   def want?(item)
     self.want_items.include?(item)
+  end
+  
+  def have(item)
+    self.haves.find_or_create_by(item_id: item.id)
+  end
+  
+  def unhave(item)
+    have = self.haves.find_by(item_id: item.id)
+    have.destroy if have
+  end
+  
+  def have?(item)
+    self.have_items.include?(item)
   end
   
 end
