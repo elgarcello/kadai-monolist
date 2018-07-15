301e06441e7a91721f0258b99475f1b85be21005
elgar_cello_du_pre <du_pre_elgar@asagi.waseda.jp>
2018-07-15 09:38:48 +0000

    have ranking

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   get 'rankings/want', to: 'rankings#want'
-
+  get 'rankings/have', to: 'rankings#have'
   
   resources :users, only: [:show, :new, :create]
   resources :items, only: [:new, :show]
